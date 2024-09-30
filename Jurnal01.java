acd4aa20f1cd36257087a49f856d65d96c87421c
Y4zidd <yazidistiqlaladhy@student.telkomuniversity.ac.id>
2024-09-30 21:17:27 +0700

    add jurnal 1

---
+++ Jurnal01.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Jurnal01 {
+
+	public static void main(final String[] args) {
+		// Kerjakan soalnya di sini
+		Scanner inputan = new Scanner(System.in);
+
+		//masukan nilai yang ingin di kuadratkan
+		int S = inputan.nextInt();
+		int N = S * S;
+
+		//hasil dari perpangkatan 2 nya
+		System.out.println(N);
+	}
+}
